ff33b45c663c8b105e7c45de5ec48e4d2bef75e9
Hazem Abdulmoneim <hazem.mando408@gmail.com>
2025-01-09 11:09:32 +0200

    some refactoring

--- heatmap.js
+++ heatmap.js
@@ -1,12 +1,9 @@
-const containers = document.getElementsByClassName("year");
-let divs = document.getElementsByClassName("entry");
 const newHabitButton = document.getElementById("addNewHabit");
 const newHabitForm = document.getElementById("newHabitForm");
 const overlay = document.getElementById("overlay");
 const formButton = document.getElementById("formButton");
 
 let habits = [];
-let entries = [];
 Entry.highestWeightEntry = null;
 
 newHabitButton.addEventListener("click", function (e) {
@@ -21,34 +18,38 @@ newHabitButton.addEventListener("click", function (e) {
 
 formButton.addEventListener("click", function (e) {
   e.preventDefault();
+  const habitName = document.getElementById("habitName");
+  const habitTrackingStyle = document.getElementById("habitTrackingStyle");
   overlay.style.display = "none";
   newHabitForm.style.visibility = "hidden";
+  const newHabit = new Habit(habitName.value, habitTrackingStyle.value);
+  const year = document.createElement("div");
+  year.classList.add("year");
+
+  for (let i = 0; i < 365; i++) {
+    newHabit.entries.push(new Entry("entry", 0));
+    let entry = document.createElement("div");
+    entry.classList.add("entry");
+    entry.addEventListener("click", function (event) {
+      let val = prompt("val: ");
+      newHabit.entries[i].value = val;
+      Entry.updateWeights();
+      for (let i = 0; i < year.children.length; i++) {
+        newHabit.entries[i].weight = newHabit.entries[i].getWeight();
+        year.children[i].style.backgroundColor = `rgba(255,69,59,${entries[
+          i
+        ].getWeight()})`;
+      }
+    });
+    year.appendChild(entry);
+  }
+  console.log(year);
+  document.body.appendChild(year);
 });
 
-habits.forEach((habit) => {});
-for (let i = 0; i < 365; i++) {
-  entries.push(new Entry("entry", 0));
-  let div = document.createElement("div");
-  div.classList.add("entry");
-  div.addEventListener("click", function (event) {
-    let name = prompt("Name: ");
-    let val = prompt("Value: ");
-    entries[i].name = name;
-    entries[i].value = val;
-    Entry.updateWeights();
-  });
-  containers[0].appendChild(div);
-}
-
-function Entry(name, value) {
+function Entry(value) {
   var weight;
   Object.defineProperties(this, {
-    name: {
-      value: name,
-      writable: true,
-      configurable: true,
-      enumerable: true,
-    },
     value: {
       value: value,
       writable: true,
@@ -67,12 +68,13 @@ function Entry(name, value) {
     },
   });
   if (Entry.highestWeightEntry == null) {
-    Entry.highestWeightEntry ??= this;
+    Entry.highestWeightEntry = this;
     this.setWeight(1);
   }
 }
 
 Entry.updateWeights = function () {
+  let divs = document.getElementsByClassName("entry");
   entries.forEach((entry) => {
     if (parseInt(entry.value) > Entry.highestWeightEntry.value) {
       Entry.highestWeightEntry.setWeight(
@@ -83,11 +85,6 @@ Entry.updateWeights = function () {
       Entry.updateWeights();
     } else {
       entry.setWeight(entry.value / Entry.highestWeightEntry.value);
-    }
-    for (let i = 0; i < divs.length; i++) {
-      divs[i].style.backgroundColor = `rgba(255,69,59,${entries[
-        i
-      ].getWeight()})`;
     }
   });
 };
@@ -104,6 +101,14 @@ function Habit(name, trackingStyle) {
     },
     entries: {
       value: new Array(365),
+      writable: true,
+    },
+    currentStreak: {
+      value: 0,
+      writable: true,
+    },
+    longestStreak: {
+      value: 0,
       writable: true,
     },
   });
