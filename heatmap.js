caf5145fe999295a48104b3e63791acfa9ed5d4c
Hazem Abdulmoneim <hazem.mando408@gmail.com>
2025-01-12 21:31:21 +0200

    set up local storage, and added the form for new entries

--- heatmap.js
+++ heatmap.js
@@ -5,9 +5,23 @@ const formButton = document.getElementById("formButton");
 const habitName = document.getElementById("habitName");
 const habitTrackingStyle = document.getElementById("habitTrackingStyle");
 
-let habits = [];
+const newEntryForm = document.getElementById("newEntry");
+const entryHabitName = document.getElementById("entryHabitName");
+const currentStreakValue = document.getElementById("currentStreakValue");
+const dateValue = document.getElementById("dateValue");
+const minutes = document.getElementById("minutes");
+const saveEntry = document.getElementById("saveEntry");
 
-function Entry(value, containingHabit) {
+let entryForm;
+let habits;
+if (localStorage.length != 0) {
+  habits = JSON.parse(localStorage.getItem("habits"));
+  habits.forEach((habit) => displayHabit(habit));
+} else {
+  habits = [];
+}
+
+function Entry(value, containingHabit, date) {
   var weight = 0;
   Object.defineProperties(this, {
     value: {
@@ -29,11 +43,14 @@ function Entry(value, containingHabit) {
         return weight;
       },
     },
+    date: {
+      value: date,
+      writable: false,
+    },
   });
 }
 
 Entry.updateWeights = function (heatmap, habit) {
-  console.log(habit);
   habit.entries.forEach((entry) => {
     if (parseInt(entry.value) > habit.highestWeightEntry.value) {
       habit.highestWeightEntry.setWeight(
@@ -59,99 +76,131 @@ function Habit(name, trackingStyle) {
     name: {
       value: name,
       writable: true,
+      enumerable: true,
     },
     trackingStyle: {
       value: trackingStyle,
       writable: false,
+      enumerable: true,
     },
     entries: {
       value: new Array(365),
       writable: true,
+      enumerable: true,
     },
     avgTime: {
       value: 0,
       writable: true,
+      enumerable: true,
     },
     currentStreak: {
       value: 0,
       writable: true,
+      enumerable: true,
     },
     longestStreak: {
       value: 0,
       writable: true,
+      enumerable: true,
     },
     highestWeightEntry: {
       value: null,
       writable: true,
+      enumerable: true,
     },
     updateCurrentStreak: {
       value: function () {},
+      enumerable: true,
     },
     updateLongestStreak: {
       value: function () {},
+      enumerable: true,
     },
   });
 
+  let startDate = new Date(2025, 0, 1);
+
   for (let i = 0; i < this.entries.length; i++) {
-    this.entries[i] = new Entry(0, this);
+    let currentDate = new Date(startDate);
+    currentDate.setDate(startDate.getDate() + i);
+
+    console.log(currentDate);
+    this.entries[i] = new Entry(0, this, currentDate.toLocaleDateString());
   }
 
   this.highestWeightEntry = this.entries[0];
   this.entries[0].setWeight(1);
 }
 
-newHabitButton.addEventListener("click", showOverlay);
+newHabitButton.addEventListener("click", function () {
+  show(overlay);
+  show(newHabitForm);
+});
 
-overlay.addEventListener("click", hideOverlay);
+overlay.addEventListener("click", function () {
+  hide(newHabitForm);
+  hide(overlay);
+  hide(newEntryForm);
+});
 
-function hideOverlay() {
-  overlay.style.display = "none";
-  newHabitForm.style.display = "none";
+function hide(element) {
+  element.style.display = "none";
 }
-function showOverlay() {
-  overlay.style.display = "block";
-  newHabitForm.style.display = "block";
+
+function show(element) {
+  element.style.display = "block";
 }
 
 formButton.addEventListener("click", newHabitHandler);
 
-(function () {
-  const habitContainer = document.createElement("div");
-  const newHabitName = document.createElement("div");
-  const newHabitAverageTime = document.createElement("span");
-  const newHabitCurrentStreak = document.createElement("span");
-  const newHabitLongestStreak = document.createElement("span");
-  const infoContainer = document.createElement("div");
+// (function () {
+//   const habitContainer = document.createElement("div");
+//   const newHabitName = document.createElement("div");
+//   const newHabitAverageTime = document.createElement("span");
+//   const newHabitCurrentStreak = document.createElement("span");
+//   const newHabitLongestStreak = document.createElement("span");
+//   const infoContainer = document.createElement("div");
 
-  const newHabit = new Habit(habitName.value, habitTrackingStyle.value);
-  habits.push(newHabit);
-  newHabitName.innerText = "Meditate";
-  newHabitAverageTime.innerText = `Average Time: 25`;
-  newHabitCurrentStreak.innerText = `Current Streak: 9 days`;
-  newHabitLongestStreak.innerText = `Longest Streak: 30 days`;
+//   const newHabit = new Habit("Meditation", "numerical");
+//   habits.push(newHabit);
 
-  habitContainer.setAttribute("id", `${newHabit.name}Container`);
-  habitContainer.classList.add("habitContainer");
-  infoContainer.classList.add("infoContainer");
-  habitContainer.appendChild(newHabitName);
-  createHeatmap(newHabit, habitContainer);
-  infoContainer.appendChild(newHabitAverageTime);
-  infoContainer.appendChild(newHabitCurrentStreak);
-  infoContainer.appendChild(newHabitLongestStreak);
+//   newHabitName.innerText = "Meditation";
+//   newHabitAverageTime.innerText = `Average Time: 25`;
+//   newHabitCurrentStreak.innerText = `Current Streak: 9 days`;
+//   newHabitLongestStreak.innerText = `Longest Streak: 30 days`;
 
-  habitContainer.appendChild(infoContainer);
+//   habitContainer.setAttribute("id", `${newHabit.name}Container`);
+//   habitContainer.classList.add("habitContainer");
+//   infoContainer.classList.add("infoContainer");
+//   habitContainer.appendChild(newHabitName);
+//   createHeatmap(newHabit, habitContainer);
+//   infoContainer.appendChild(newHabitAverageTime);
+//   infoContainer.appendChild(newHabitCurrentStreak);
+//   infoContainer.appendChild(newHabitLongestStreak);
 
-  newHabitName.classList.add("habitName");
-  newHabitAverageTime.classList.add("info");
-  newHabitCurrentStreak.classList.add("info");
-  newHabitLongestStreak.classList.add("info");
+//   habitContainer.appendChild(infoContainer);
 
-  document.body.appendChild(habitContainer);
-})();
+//   newHabitName.classList.add("habitName");
+//   newHabitAverageTime.classList.add("info");
+//   newHabitCurrentStreak.classList.add("info");
+//   newHabitLongestStreak.classList.add("info");
+
+//   document.body.appendChild(habitContainer);
+// })();
+
 function newHabitHandler(e) {
   e.preventDefault();
-  hideOverlay();
+  hide(overlay);
+  hide(newHabitForm);
 
+  const newHabit = new Habit(habitName.value, habitTrackingStyle.value);
+  habits.push(newHabit);
+  localStorage.setItem("habits", JSON.stringify(habits));
+
+  displayHabit(newHabit);
+}
+
+function displayHabit(habit) {
   const habitContainer = document.createElement("div");
   const newHabitName = document.createElement("div");
   const newHabitAverageTime = document.createElement("span");
@@ -159,25 +208,21 @@ function newHabitHandler(e) {
   const newHabitLongestStreak = document.createElement("span");
   const infoContainer = document.createElement("div");
 
-  const newHabit = new Habit(habitName.value, habitTrackingStyle.value);
-  habits.push(newHabit);
-  newHabitName.innerHTML = habitName.value;
+  newHabitName.innerHTML = habit.name;
+  newHabitAverageTime.innerText = `Average Time: ${habit.avgTime}`;
+  newHabitCurrentStreak.innerText = `Current Streak: ${habit.currentStreak}`;
+  newHabitLongestStreak.innerText = `Longest Streak: ${habit.longestStreak}`;
 
-  newHabitAverageTime.innerText = `Average Time: ${newHabit.avgTime}`;
-  newHabitCurrentStreak.innerText = `Current Streak: ${newHabit.currentStreak}`;
-  newHabitLongestStreak.innerText = `Longest Streak: ${newHabit.longestStreak}`;
-
-  habitContainer.setAttribute("id", `${newHabit.name}Container`);
+  habitContainer.setAttribute("id", `${habit.name}Container`);
   habitContainer.classList.add("habitContainer");
   newHabitName.classList.add("habitName");
   infoContainer.classList.add("infoContainer");
 
+  habitContainer.appendChild(newHabitName);
+  createHeatmap(habit, habitContainer);
   infoContainer.appendChild(newHabitAverageTime);
   infoContainer.appendChild(newHabitCurrentStreak);
   infoContainer.appendChild(newHabitLongestStreak);
-
-  habitContainer.appendChild(newHabitName);
-  createHeatmap(newHabit, habitContainer);
   habitContainer.appendChild(infoContainer);
 
   document.body.appendChild(habitContainer);
@@ -190,17 +235,27 @@ function createHeatmap(habit, container) {
     let entryDiv = document.createElement("div");
     entryDiv.classList.add("entry");
     entryDiv.addEventListener("click", function () {
-      let val = prompt("val: ");
-      entry.value = val;
-      Entry.updateWeights(container.children[1], habit);
-      // for (let i = 0; i < year.children.length; i++) {
-      //   newHabit.entries[i].weight = newHabit.entries[i].getWeight();
-      //   year.children[i].style.backgroundColor = `rgba(255,69,59,${entries[
-      //     i
-      //   ].getWeight()})`;
-      // }
+      show(overlay);
+      showEntryForm(habit, entry, container);
     });
     habitHeatmap.appendChild(entryDiv);
   });
   container.appendChild(habitHeatmap);
+}
+
+function showEntryForm(habit, entry, container) {
+  entryHabitName.innerText = habit.name;
+  dateValue.innerText = entry.date;
+  minutes.innerText = entry.value;
+  currentStreakValue.innerText = habit.currentStreak;
+
+  saveEntry.addEventListener("click", function (event) {
+    event.preventDefault();
+    hide(overlay);
+    hide(newEntryForm);
+    entry.value = minutes.value;
+    Entry.updateWeights(container.children[1], habit);
+  });
+
+  show(newEntryForm);
 }
